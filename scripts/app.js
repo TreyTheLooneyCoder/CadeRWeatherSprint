a8a154fe08683489ac448decdfcdce085cb5d5b1
TreyTheLooneyCoder <roytreslll@gmail.com>
2024-12-10 13:31:11 -0800

    - API's called properly

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,26 @@
 import { APIKEY } from "./environment.js";
 
-function apiCall () {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=37.9577&lon=-121.2908&appid=${APIKEY}`)
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        console.log(data);
-    })
+async function weatherCall() {
+    const promise = await fetch("../https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}");
+    const weather = await promise.json();
+    console.log(weather);
+    return weather;
 }
 
-apiCall();
+async function forecastCall() {
+    const promise = await fetch("../https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}");
+    const forecast = await promise.json();
+    console.log(forecast);
+    return forecast;
+}
+
+async function geoCall() {
+    const promise = await fetch("../https://http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}");
+    const geoLocate = await promise.json();
+    console.log(geoLocate);
+    return geoLocate;
+}
+
+weatherCall();
+forecastCall();
+geoCall();
